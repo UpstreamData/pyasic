869d782f82e16baa8bf6ed77db54a9f13f178a66
UpstreamData <brett@upstreamdata.ca>
2021-11-29 14:49:25 -0700

    added a file to make executables of the cfg util

---
+++ make_cfg_tool_exe.py
@@ -0,0 +1,27 @@
+"""
+Make a build of the config tool.
+
+Usage: make_config_tool.py build
+
+The build will show up in the build directory.
+"""
+import datetime
+import sys
+import os
+
+from cx_Freeze import setup, Executable
+
+base = None
+if sys.platform == "win32":
+    base = "Win32GUI"
+
+
+version = datetime.datetime.now()
+version = version.strftime("%y.%m.%d")
+print(version)
+setup(name="UpstreamCFGUtil.exe",
+      version=version,
+      description="Upstream Data Config Utility Build",
+      options={"build_exe":{"build_exe": f"{os.getcwd()}\\build\\UpstreamCFGUtil-{version}-{sys.platform}\\"}},
+      executables=[Executable("config_tool.py", base=base, icon="icon.ico", target_name="UpstreamCFGUtil.exe")]
+      )
