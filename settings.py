66d2b63ae695a780882ef61c97277b8330d8dbe6
UpstreamData <brett@upstreamdata.ca>
2021-12-22 12:39:20 -0700

    added settings.toml file and used settings.py to get data from it

--- settings.py
+++ settings.py
@@ -1,7 +1,21 @@
-NETWORK_PING_RETRIES: int = 3
-NETWORK_PING_TIMEOUT: int = 5
-NETWORK_SCAN_THREADS: int = 300
+import toml
+import os
 
-CFG_UTIL_CONFIG_THREADS: int = 300
+try:
+    with open(os.path.join(os.getcwd(), "settings.toml"), "r") as settings_file:
+        settings = toml.loads(settings_file.read())
+    NETWORK_PING_RETRIES: int = settings["ping_retries"]
+    NETWORK_PING_TIMEOUT: int = settings["ping_timeout"]
+    NETWORK_SCAN_THREADS: int = settings["scan_threads"]
 
-MINER_FACTORY_GET_VERSION_RETRIES: int = 3
+    CFG_UTIL_CONFIG_THREADS: int = settings["config_threads"]
+
+    MINER_FACTORY_GET_VERSION_RETRIES: int = settings["get_version_retries"]
+except:
+    NETWORK_PING_RETRIES: int = 3
+    NETWORK_PING_TIMEOUT: int = 5
+    NETWORK_SCAN_THREADS: int = 300
+
+    CFG_UTIL_CONFIG_THREADS: int = 300
+
+    MINER_FACTORY_GET_VERSION_RETRIES: int = 3
