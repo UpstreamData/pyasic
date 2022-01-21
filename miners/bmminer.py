f46b39c9878f89a332b64cdca890978ff292f6eb
UpstreamData <brett@upstreamdata.ca>
2022-01-21 16:15:46 -0700

    added Hive get bad boards, and started on a bad board utility

--- miners/bmminer.py
+++ miners/bmminer.py
@@ -1,12 +1,16 @@
 from API.bmminer import BMMinerAPI
 from miners import BaseMiner
-
+import asyncssh
 
 class BMMiner(BaseMiner):
     def __init__(self, ip: str) -> None:
         api = BMMinerAPI(ip)
-        self.model = None
         super().__init__(ip, api)
+        self.model = None
+        self.config = None
+        self.uname = 'root'
+        self.pwd = 'admin'
+
 
     def __repr__(self) -> str:
         return f"BMMiner: {str(self.ip)}"
@@ -21,16 +25,58 @@ class BMMiner(BaseMiner):
         return None
 
     async def get_hostname(self) -> str:
-        return "?"
+        try:
+            async with (await self._get_ssh_connection()) as conn:
+                if conn is not None:
+                    data = await conn.run('cat /proc/sys/kernel/hostname')
+                    return data.stdout.strip()
+                else:
+                    return "?"
+        except Exception:
+            return "?"
+
+    async def _get_ssh_connection(self) -> asyncssh.connect:
+        try:
+            conn = await asyncssh.connect(str(self.ip),
+                                          known_hosts=None,
+                                          username=self.uname,
+                                          password=self.pwd,
+                                          server_host_key_algs=['ssh-rsa'])
+            return conn
+        except asyncssh.misc.PermissionDenied:
+            try:
+                conn = await asyncssh.connect(str(self.ip),
+                                              known_hosts=None,
+                                              username="admin",
+                                              password="admin",
+                                              server_host_key_algs=['ssh-rsa'])
+                return conn
+            except Exception as e:
+                print(e)
+        except OSError:
+            print(str(self.ip) + " Connection refused.")
+            return None
+
+    async def send_ssh_command(self, cmd):
+        result = None
+        async with (await self._get_ssh_connection()) as conn:
+            for i in range(3):
+                try:
+                    result = await conn.run(cmd)
+                except Exception as e:
+                    print(f"{cmd} error: {e}")
+                    if i == 3:
+                        return
+                    continue
 
     async def send_config(self, _):
         return None  # ignore for now
 
     async def restart_backend(self) -> None:
-        return None  # Murray
+        return None
 
     async def reboot(self) -> None:
-        return None  # Murray
+        await self.send_ssh_command("reboot")
 
     async def get_config(self) -> None:
-        return None  # Murray
+        return None
---
+++ tools/bad_board_util/miner_factory.py
@@ -0,0 +1,7 @@
+"""
+This file stores the MinerFactory instance used by the BadBoardUtility for use in other files.
+"""
+
+from miners.miner_factory import MinerFactory
+
+miner_factory = MinerFactory()
