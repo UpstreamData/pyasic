a1839aae46ec83e87af2098a39a51c39799da0e0
UpstreamData <brett@upstreamdata.ca>
2022-01-21 14:42:01 -0700

    added T9s

---
+++ miners/antminer/T9/cgminer.py
@@ -0,0 +1,11 @@
+from miners.cgminer import CGMiner
+
+
+class CGMinerT9(CGMiner):
+    def __init__(self, ip: str) -> None:
+        super().__init__(ip)
+        self.model = "T9"
+        self.api_type = "CGMiner"
+
+    def __repr__(self) -> str:
+        return f"CGMinerT9: {str(self.ip)}"
