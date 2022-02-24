ae8b2cbd074fcc92a16f4faee1fc2d3d855afb47
UpstreamData <brett@upstreamdata.ca>
2022-02-24 15:25:49 -0700

    added the required directories for settings and scanning

--- tools/web_monitor/app.py
+++ tools/web_monitor/app.py
@@ -9,5 +9,7 @@ sio.attach(app)
 
 app.static('/', "./public/index.html")
 app.static('/index.css', "./static/index.css")
+app.static('/index.js', "./static/index.js")
 
 app.static('/scan', "./public/scan.html")
+app.static('/settings', "./public/settings.html")
