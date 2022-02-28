2e76ecdc1e2e6dae80700c0fd77967c4d8518320
UpstreamData <brett@upstreamdata.ca>
2022-02-28 15:15:57 -0700

    added add selected miners button

--- tools/web_monitor/app.py
+++ tools/web_monitor/app.py
@@ -25,37 +25,63 @@ def scan(request: Request):
     return templates.TemplateResponse("scan.html", {"request": request})
 
 
+@app.post("/scan/add_miners")
+def add_miners_scan(request: Request):
+    print(request)
+    return scan
+
+
 @app.websocket("/scan/ws")
 async def websocket_scan(websocket: WebSocket):
     await websocket.accept()
+    cur_task = None
     try:
         while True:
-            network_ip = await websocket.receive_text()
-            if "/" in network_ip:
-                network_ip, network_subnet = network_ip.split("/")
-                network = MinerNetwork(network_ip, mask=network_subnet)
+            ws_data = await websocket.receive_text()
+            if "-Cancel-" in ws_data:
+                if cur_task:
+                    cur_task.cancel()
+                    try:
+                        await cur_task
+                    except asyncio.CancelledError:
+                        cur_task = None
+                await websocket.send_text("Cancelled")
             else:
-                network = MinerNetwork(network_ip)
-            miner_generator = network.scan_network_generator()
-            miners = []
-            async for miner in miner_generator:
-                if miner:
-                    miners.append(miner)
-
-            get_miner_genenerator = miner_factory.get_miner_generator(miners)
-            all_miners = []
-            async for found_miner in get_miner_genenerator:
-                all_miners.append({"ip": found_miner.ip, "model": await found_miner.get_model()})
-                all_miners.sort(key=lambda x: x["ip"])
-                send_miners = []
-                for miner in all_miners:
-                    send_miners.append({"ip": str(miner["ip"]), "model": miner["model"]})
-                await websocket.send_json(send_miners)
-            await websocket.send_text("Done")
+                cur_task = asyncio.create_task(do_websocket_scan(websocket, ws_data))
+            if cur_task and cur_task.done():
+                cur_task = None
     except WebSocketDisconnect:
         print("Websocket disconnected.")
         pass
 
+
+async def do_websocket_scan(websocket: WebSocket, network_ip: str):
+    try:
+        if "/" in network_ip:
+            network_ip, network_subnet = network_ip.split("/")
+            network = MinerNetwork(network_ip, mask=network_subnet)
+        else:
+            network = MinerNetwork(network_ip)
+        miner_generator = network.scan_network_generator()
+        miners = []
+        async for miner in miner_generator:
+            if miner:
+                miners.append(miner)
+
+        get_miner_genenerator = miner_factory.get_miner_generator(miners)
+        all_miners = []
+        async for found_miner in get_miner_genenerator:
+            all_miners.append(
+                {"ip": found_miner.ip, "model": await found_miner.get_model()})
+            all_miners.sort(key=lambda x: x["ip"])
+            send_miners = []
+            for miner in all_miners:
+                send_miners.append(
+                    {"ip": str(miner["ip"]), "model": miner["model"]})
+            await websocket.send_json(send_miners)
+        await websocket.send_text("Done")
+    except asyncio.CancelledError:
+        raise
 
 if __name__ == "__main__":
     uvicorn.run("app:app", host="127.0.0.1", port=80)
