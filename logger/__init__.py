0b82b9fc25cc647b23b06bf4a0102f40ab00f7e8
UpstreamData <brett@upstreamdata.ca>
2022-03-16 14:03:32 -0600

    improved logging format and sent output to a file

--- logger/__init__.py
+++ logger/__init__.py
@@ -2,13 +2,17 @@ import logging
 from settings import DEBUG
 
 
-logging.basicConfig()
+logging.basicConfig(
+    filename="logfile.txt",
+    filemode="a",
+    format='[%(levelname)s][%(asctime)s](%(name)s) - %(message)s',
+    datefmt='%x %X'
+)
 logger = logging.getLogger()
 
 if DEBUG:
     logger.setLevel(logging.DEBUG)
-    logging.getLogger("asyncssh").setLevel(logging.WARNING)
-
+    logging.getLogger("asyncssh").setLevel(logging.DEBUG)
 else:
     logger.setLevel(logging.INFO)
     logging.getLogger("asyncssh").setLevel(logging.WARNING)
