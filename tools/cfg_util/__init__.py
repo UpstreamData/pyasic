ef336a9e23fc0430c3a79a7e49c81a660be4d3e0
UpstreamData <brett@upstreamdata.ca>
2022-05-09 09:20:21 -0600

    added asyncio event loop policy update to fix some bugs

--- tools/cfg_util/__init__.py
+++ tools/cfg_util/__init__.py
@@ -1,5 +1,15 @@
-from .ui import ui
 import asyncio
+import sys
+
+from .ui import ui
+
+# Fix bug with some whatsminers and asyncio because of a socket not being shut down:
+if (
+    sys.version_info[0] == 3
+    and sys.version_info[1] >= 8
+    and sys.platform.startswith("win")
+):
+    asyncio.set_event_loop_policy(asyncio.WindowsSelectorEventLoopPolicy())
 
 
 def main():
