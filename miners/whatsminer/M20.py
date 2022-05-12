0eb7ced932b88057ea1572cb446421fcf5b44ed2
UpstreamData <brett@upstreamdata.ca>
2022-05-12 13:20:57 -0600

    added btminer board data

--- miners/whatsminer/M20.py
+++ miners/whatsminer/M20.py
@@ -4,6 +4,7 @@ from miners.btminer import BTMiner
 class BTMinerM20(BTMiner):
     def __init__(self, ip: str) -> None:
         super().__init__(ip)
+        self.nominal_chips = 66
 
     def __repr__(self) -> str:
         return f"M20 - BTMiner: {str(self.ip)}"
