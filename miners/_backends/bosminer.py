5e6bc8c8ef14582da69341495284ec7903d496e3
UpstreamData <brett@upstreamdata.ca>
2022-05-17 08:51:56 -0600

    add mac addresses for bosminer, and reformat

--- miners/_backends/bosminer.py
+++ miners/_backends/bosminer.py
@@ -4,7 +4,6 @@ import toml
 from config.bos import bos_config_convert, general_config_convert_bos
 import logging
 from settings import MINER_FACTORY_GET_VERSION_RETRIES as DATA_RETRIES
-import asyncssh
 
 
 class BOSMiner(BaseMiner):
@@ -371,41 +370,6 @@ class BOSMiner(BaseMiner):
 
         return data
 
-
-class BOSMinerOld(BaseMiner):
-    def __init__(self, ip: str) -> None:
-        super().__init__(ip)
-        self.api = BOSMinerAPI(ip)
-        self.api_type = "BOSMiner"
-        self.uname = "root"
-        self.pwd = "admin"
-
-    async def send_ssh_command(self, cmd: str) -> str or None:
-        """Send a command to the miner over ssh.
-
-        :return: Result of the command or None.
-        """
-        result = None
-
-        # open an ssh connection
-        async with await asyncssh.connect("192.168.1.11", username="root") as conn:
-            # 3 retries
-            for i in range(3):
-                try:
-                    # run the command and get the result
-                    result = await conn.run(cmd)
-                    result = result.stdout
-                except Exception as e:
-                    # if the command fails, log it
-                    logging.warning(f"{self} command {cmd} error: {e}")
-
-                    # on the 3rd retry, return None
-                    if i == 3:
-                        return
-                    continue
-        # return the result, either command output or None
-        return str(result)
-
-    async def update_to_plus(self):
-        result = await self.send_ssh_command("opkg update && opkg install bos_plus")
-        return result
+    async def get_mac(self):
+        result = await self.send_ssh_command("cat /sys/class/net/eth0/address")
+        return result.upper()
