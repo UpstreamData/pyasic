c3e285a9ee4e9e579166155f7bd996a0c2ffae0c
UpstreamData <brett@upstreamdata.ca>
2022-05-18 11:06:21 -0600

    fix some bugs in web monitor

--- tools/web_monitor/func/__init__.py
+++ tools/web_monitor/func/__init__.py
@@ -1,11 +1,13 @@
 import os
 import ipaddress
 
+dir_path = "\\".join(os.path.dirname(os.path.realpath(__file__)).split("\\")[:-1])
+
 
 def get_current_miner_list():
     cur_miners = []
-    if os.path.exists(os.path.join(os.getcwd(), "miner_list.txt")):
-        with open(os.path.join(os.getcwd(), "miner_list.txt")) as file:
+    if os.path.exists(os.path.join(dir_path, "miner_list.txt")):
+        with open(os.path.join(dir_path, "miner_list.txt")) as file:
             for line in file.readlines():
                 cur_miners.append(line.strip())
     cur_miners = sorted(cur_miners, key=lambda x: ipaddress.ip_address(x))
--- tools/web_monitor/scan/__init__.py
+++ tools/web_monitor/scan/__init__.py
@@ -5,6 +5,10 @@ from tools.web_monitor.func import get_current_miner_list
 
 from .ws import router as ws_router
 
+import os
+
+dir_path = "\\".join(os.path.dirname(os.path.realpath(__file__)).split("\\")[:-1])
+
 router = APIRouter()
 router.include_router(ws_router)
 
@@ -19,7 +23,7 @@ def scan(request: Request):
 @router.post("/add_miners")
 async def add_miners_scan(request: Request):
     miners = await request.json()
-    with open("miner_list.txt", "a+") as file:
+    with open(os.path.join(dir_path, "miner_list.txt"), "a+") as file:
         for miner_ip in miners["miners"]:
             file.write(miner_ip + "\n")
     return scan
