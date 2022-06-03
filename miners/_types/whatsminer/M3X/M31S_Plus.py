7a5a0b287c6d4d03d70bfda9b65c06bbf7a544b6
UpstreamData <brett@upstreamdata.ca>
2022-06-03 09:35:55 -0600

    fixed a bug with some versions of whatsminer and improved logging

--- miners/_types/whatsminer/M3X/M31S_Plus.py
+++ miners/_types/whatsminer/M3X/M31S_Plus.py
@@ -8,3 +8,12 @@ class M31SPlus(BaseMiner):
         self.model = "M31S+"
         self.nominal_chips = 78
         self.fan_count = 2
+
+
+class M31SPlusVE20(BaseMiner):
+    def __init__(self, ip: str):
+        super().__init__()
+        self.ip = ip
+        self.model = "M31S+VE20"
+        self.nominal_chips = 78
+        self.fan_count = 2
--- miners/_types/whatsminer/M3X/__init__.py
+++ miners/_types/whatsminer/M3X/__init__.py
@@ -3,6 +3,6 @@ from .M30S_Plus import M30SPlus
 from .M30S_Plus_Plus import M30SPlusPlusVG30, M30SPlusPlusVG40
 
 from .M31S import M31S
-from .M31S_Plus import M31SPlus
+from .M31S_Plus import M31SPlus, M31SPlusVE20
 
 from .M32S import M32S
