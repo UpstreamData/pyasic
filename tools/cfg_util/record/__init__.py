8932f8d7624a0dd5a036452f5d68ecd5d9c728ca
upstreamdata <bret@upstreamdata.ca>
2022-07-02 18:49:32 -0600

    changed over to package format and removed tools, added poetry

--- tools/cfg_util/record/__init__.py
+++
@@ -1,1 +0,0 @@
-from tools.cfg_util.record.ui import record_ui
--- tools/web_monitor/template.py
+++
@@ -1,7 +0,0 @@
-import os
-from fastapi.templating import Jinja2Templates
-
-
-templates = Jinja2Templates(
-    directory=os.path.join(os.path.dirname(__file__), "templates")
-)
