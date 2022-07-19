3ec833e700cb0a5c9a014043a44a14138e18b97b
UpstreamData <75442874+UpstreamData@users.noreply.github.com>
2022-07-19 15:43:24 -0600

    add copyright license, using Apache 2.0 license

--- pyasic/miners/_types/whatsminer/M3X/__init__.py
+++ pyasic/miners/_types/whatsminer/M3X/__init__.py
@@ -1,3 +1,17 @@
+#  Copyright 2022 Upstream Data Inc
+#
+#  Licensed under the Apache License, Version 2.0 (the "License");
+#  you may not use this file except in compliance with the License.
+#  You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+#  Unless required by applicable law or agreed to in writing, software
+#  distributed under the License is distributed on an "AS IS" BASIS,
+#  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#  See the License for the specific language governing permissions and
+#  limitations under the License.
+
 from .M30S import M30S, M30SVE10, M30SVE20, M30SVG20, M30SV50
 from .M30S_Plus import M30SPlus, M30SPlusVG60, M30SPlusVE40, M30SPlusVF20
 from .M30S_Plus_Plus import M30SPlusPlus, M30SPlusPlusVG30, M30SPlusPlusVG40
