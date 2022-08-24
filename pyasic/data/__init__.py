4fa3511725038a9361de2cbf49f4947b5ca369f7
UpstreamData <brett@upstreamdata.ca>
2022-08-24 13:50:55 -0600

    add float support to `MinerData().as_influx()`

--- pyasic/data/__init__.py
+++ pyasic/data/__init__.py
@@ -193,6 +193,9 @@ class MinerData:
                 field_data.append(f"{attribute}={str(self[attribute]).lower()}")
                 continue
             if isinstance(self[attribute], int):
+                field_data.append(f"{attribute}={self[attribute]}i")
+                continue
+            if isinstance(self[attribute], float):
                 field_data.append(f"{attribute}={self[attribute]}")
                 continue
             if attribute == "fault_light" and not self[attribute]:
