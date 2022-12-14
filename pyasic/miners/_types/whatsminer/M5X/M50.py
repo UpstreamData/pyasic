ec8ef328db4080e4b51126025e978b548ce12dbb
UpstreamData <brett@upstreamdata.ca>
2022-12-14 14:45:46 -0700

    fix some bosminer bugs with new make types and graphql, and rename `models.py` file to `makes.py` to be consistent with naming conventions.

--- pyasic/miners/_types/whatsminer/M5X/M50.py
+++ pyasic/miners/_types/whatsminer/M5X/M50.py
@@ -12,7 +12,7 @@
 #  See the License for the specific language governing permissions and
 #  limitations under the License.
 
-from pyasic.miners._types.models import WhatsMiner
+from pyasic.miners._types.makes import WhatsMiner
 
 
 class M50(WhatsMiner):  # noqa - ignore ABC method implementation
