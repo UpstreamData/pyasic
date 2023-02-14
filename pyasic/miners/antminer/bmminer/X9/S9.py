4c61c4c345fff705e5e0fed72524d91e2459b374
UpstreamData <brett@upstreamdata.ca>
2023-02-14 10:33:14 -0700

    bug: add MAC address support for stock S9s.

--- pyasic/miners/antminer/bmminer/X9/S9.py
+++ pyasic/miners/antminer/bmminer/X9/S9.py
@@ -14,9 +14,54 @@
 #  limitations under the License.                                              -
 # ------------------------------------------------------------------------------
 
+import json
+from typing import Optional, Union
+
+import httpx
+
 from pyasic.miners._backends import BMMiner  # noqa - Ignore access to _module
 from pyasic.miners._types import S9  # noqa - Ignore access to _module
+from pyasic.settings import PyasicSettings
 
 
 class BMMinerS9(BMMiner, S9):
-    pass
+    def __init__(self, ip: str, api_ver: str = "0.0.0") -> None:
+        super().__init__(ip, api_ver=api_ver)
+        self.ip = ip
+        self.uname = "root"
+        self.pwd = PyasicSettings().global_x19_password
+
+    async def send_web_command(
+        self, command: str, params: dict = None
+    ) -> Optional[dict]:
+        url = f"http://{self.ip}/cgi-bin/{command}.cgi"
+        auth = httpx.DigestAuth(self.uname, self.pwd)
+        try:
+            async with httpx.AsyncClient() as client:
+                if params:
+                    data = await client.post(url, data=params, auth=auth)
+                else:
+                    data = await client.get(url, auth=auth)
+        except httpx.HTTPError:
+            pass
+        else:
+            if data.status_code == 200:
+                try:
+                    return data.json()
+                except json.decoder.JSONDecodeError:
+                    pass
+
+    async def get_mac(self) -> Union[str, None]:
+        try:
+            data = await self.send_web_command("get_system_info")
+            if data:
+                return data["macaddr"]
+        except KeyError:
+            pass
+
+        try:
+            data = await self.send_web_command("get_network_info")
+            if data:
+                return data["macaddr"]
+        except KeyError:
+            pass
