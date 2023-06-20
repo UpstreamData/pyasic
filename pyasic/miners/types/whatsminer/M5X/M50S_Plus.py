c691868e9b1bd53a4fe0b001d4fa9c66bc0bd214
UpstreamData <brett@upstreamdata.ca>
2023-06-20 12:11:05 -0600

    bug: add chip count for M50S+VK20.

--- pyasic/miners/types/whatsminer/M5X/M50S_Plus.py
+++ pyasic/miners/types/whatsminer/M5X/M50S_Plus.py
@@ -60,8 +60,5 @@ class M50SPlusVK20(WhatsMiner):  # noqa - ignore ABC method implementation
         super().__init__(ip, api_ver)
         self.ip = ip
         self.model = "M50S+ VK20"
-        self.nominal_chips = 0
-        warnings.warn(
-            "Unknown chip count for miner type M50S+ VK20, please open an issue on GitHub (https://github.com/UpstreamData/pyasic)."
-        )
+        self.nominal_chips = 117
         self.fan_count = 2
