5f679fa889e6a04b16f963b55c841d0a3bb0c86e
checksum0 <checksum0@protonmail.com>
2023-12-12 22:51:06 -0300

    Ensure all new BTMiner API class are imported in __init__.py

---
+++ pyasic/miners/whatsminer/btminer/M6X/__init__.py
@@ -0,0 +1,52 @@
+# ------------------------------------------------------------------------------
+#  Copyright 2023 Upstream Data Inc                                            -
+#                                                                              -
+#  Licensed under the Apache License, Version 2.0 (the "License");             -
+#  you may not use this file except in compliance with the License.            -
+#  You may obtain a copy of the License at                                     -
+#                                                                              -
+#      http://www.apache.org/licenses/LICENSE-2.0                              -
+#                                                                              -
+#  Unless required by applicable law or agreed to in writing, software         -
+#  distributed under the License is distributed on an "AS IS" BASIS,           -
+#  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.    -
+#  See the License for the specific language governing permissions and         -
+#  limitations under the License.                                              -
+# ------------------------------------------------------------------------------
+
+from .M60 import (
+    BTMinerM60VK10,
+    BTMinerM60VK20,
+    BTMinerM60VK30,
+    BTMinerM60VK40,
+)
+
+from .M60S import (
+    BTMinerM60SVK10,
+    BTMinerM60SVK20,
+    BTMinerM60SVK30,
+    BTMinerM60SVK40,
+)
+
+from .M63 import (
+    BTMinerM63VK10,
+    BTMinerM63VK20,
+    BTMinerM63VK30,
+)
+
+from .M63S import (
+    BTMinerM63SVK10,
+    BTMinerM63SVK20,
+    BTMinerM63SVK30,
+)
+
+from .M66 import (
+    BTMinerM66VK20,
+    BTMinerM66VK30,
+)
+
+from .M66S import (
+    BTMinerM66SVK20,
+    BTMinerM66SVK30,
+    BTMinerM66SVK40,
+)
