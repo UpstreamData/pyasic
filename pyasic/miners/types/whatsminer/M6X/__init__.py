6976351ee58afa28fe4fd9c6e5938f12604654f2
checksum0 <checksum0@protonmail.com>
2023-12-12 22:40:57 -0300

    Ensure all new types are imported in their respective __init__.py

---
+++ pyasic/miners/types/whatsminer/M6X/__init__.py
@@ -0,0 +1,52 @@
+# ------------------------------------------------------------------------------
+#  Copyright 2023 Upstream Data Inc                                            -
+#                                                                              -
+#  Licensed under the Apache License, Version 2.0 (the "License");             -
+#  you may not use this file except in compliance with the License.            -
+#  You may obtain a copy of the License at                                     -
+#                                                                              -
+#      http://www.apache.org/licenses/LICENSE-2.0                              -
+#                                                                              -
+#  Unless required by applicable law or agreed to in writing, software         -
+#  distributed under the License is distributed on an "AS IS" BASIS,           -
+#  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.    -
+#  See the License for the specific language governing permissions and         -
+#  limitations under the License.                                              -
+# ------------------------------------------------------------------------------
+
+from .M60 import (
+    M60VK10,
+    M60VK20,
+    M60VK30,
+    M60VK40,
+)
+
+from .M60S import (
+    M60SVK10,
+    M60SVK20,
+    M60SVK30,
+    M60SVK40,
+)
+
+from .M63 import (
+    M63VK10,
+    M63VK20,
+    M63VK30,
+)
+
+from .M63S import (
+    M63SVK10,
+    M63SVK20,
+    M63SVK30,
+)
+
+from .M66 import (
+    M66VK20,
+    M66VK30,
+)
+
+from .M66S import (
+    M66SVK20,
+    M66SVK30,
+    M66SVK40,
+)
