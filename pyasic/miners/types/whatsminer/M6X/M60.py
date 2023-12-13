ecb0fc6db065c9d77d027d97ab658bf07e6354ca
checksum0 <checksum0@protonmail.com>
2023-12-12 22:37:15 -0300

    Add all new known types of Whatsminer M6X

---
+++ pyasic/miners/types/whatsminer/M6X/M60.py
@@ -0,0 +1,66 @@
+# ------------------------------------------------------------------------------
+#  Copyright 2023 Upstream Data Inc                                            -
+#                                                                              -
+#  Licensed under the Apache License, Version 2.0 (the "License");             -
+#  you may not use this file except in compliance with the License.            -
+#  You may obtain a copy of the License at                                     -
+#                                                                              -
+#      http://www.apache.org/licenses/LICENSE-2.0                              -
+#                                                                              -
+#  Unless required by applicable law or agreed to in writing, software         -
+#  distributed under the License is distributed on an "AS IS" BASIS,           -
+#  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.    -
+#  See the License for the specific language governing permissions and         -
+#  limitations under the License.                                              -
+# ------------------------------------------------------------------------------
+ 
+import warnings
+
+from pyasic.miners.makes import WhatsMiner
+
+class M60VK10(WhatsMiner):  # noqa - ignore ABC method implementation
+    def __init__(self, ip: str, api_ver: str = "0.0.0"):
+        super().__init__(ip, api_ver)
+        self.ip = ip
+        self.model = "M60 VK10"
+        self.nominal_chips = 0
+        warnings.warn(
+            "Unknown chip count for miner type M60 VK10, please open an issue on GitHub (https://github.com/UpstreamData/pyasic)."
+        )
+        self.fan_count = 2
+
+
+class M60VK20(WhatsMiner):  # noqa - ignore ABC method implementation
+    def __init__(self, ip: str, api_ver: str = "0.0.0"):
+        super().__init__(ip, api_ver)
+        self.ip = ip
+        self.model = "M60 VK20"
+        self.nominal_chips = 0
+        warnings.warn(
+            "Unknown chip count for miner type M60 VK20, please open an issue on GitHub (https://github.com/UpstreamData/pyasic)."
+        )
+        self.fan_count = 2
+
+
+class M60VK30(WhatsMiner):  # noqa - ignore ABC method implementation
+    def __init__(self, ip: str, api_ver: str = "0.0.0"):
+        super().__init__(ip, api_ver)
+        self.ip = ip
+        self.model = "M60 VK30"
+        self.nominal_chips = 0
+        warnings.warn(
+            "Unknown chip count for miner type M60 VK30, please open an issue on GitHub (https://github.com/UpstreamData/pyasic)."
+        )
+        self.fan_count = 2
+
+
+class M60VK40(WhatsMiner):  # noqa - ignore ABC method implementation
+    def __init__(self, ip: str, api_ver: str = "0.0.0"):
+        super().__init__(ip, api_ver)
+        self.ip = ip
+        self.model = "M60 VK40"
+        self.nominal_chips = 0
+        warnings.warn(
+            "Unknown chip count for miner type M60 VK40, please open an issue on GitHub (https://github.com/UpstreamData/pyasic)."
+        )
+        self.fan_count = 2
---
+++ pyasic/miners/types/whatsminer/M6X/M63S.py
@@ -0,0 +1,52 @@
+# ------------------------------------------------------------------------------
+#  Copyright 2023 Upstream Data Inc                                            -
+#                                                                              -
+#  Licensed under the Apache License, Version 2.0 (the "License");             -
+#  you may not use this file except in compliance with the License.            -
+#  You may obtain a copy of the License at                                     -
+#                                                                              -
+#      http://www.apache.org/licenses/LICENSE-2.0                              -
+#                                                                              -
+#  Unless required by applicable law or agreed to in writing, software         -
+#  distributed under the License is distributed on an "AS IS" BASIS,           -
+#  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.    -
+#  See the License for the specific language governing permissions and         -
+#  limitations under the License.                                              -
+# ------------------------------------------------------------------------------
+ 
+import warnings
+
+from pyasic.miners.makes import WhatsMiner
+
+class M63SVK10(WhatsMiner):  # noqa - ignore ABC method implementation
+    def __init__(self, ip: str, api_ver: str = "0.0.0"):
+        super().__init__(ip, api_ver)
+        self.ip = ip
+        self.model = "M63S VK10"
+        self.nominal_chips = 0
+        warnings.warn(
+            "Unknown chip count for miner type M63S VK10, please open an issue on GitHub (https://github.com/UpstreamData/pyasic)."
+        )
+        self.fan_count = 2
+
+
+class M63SVK20(WhatsMiner):  # noqa - ignore ABC method implementation
+    def __init__(self, ip: str, api_ver: str = "0.0.0"):
+        super().__init__(ip, api_ver)
+        self.ip = ip
+        self.model = "M63S VK20"
+        self.nominal_chips = 0
+        warnings.warn(
+            "Unknown chip count for miner type M63S VK20, please open an issue on GitHub (https://github.com/UpstreamData/pyasic)."
+        )
+        self.fan_count = 2
+
+
+class M63SVK30(WhatsMiner):  # noqa - ignore ABC method implementation
+    def __init__(self, ip: str, api_ver: str = "0.0.0"):
+        super().__init__(ip, api_ver)
+        self.ip = ip
+        self.model = "M63S VK30"
+        self.nominal_chips = 370
+        self.ideal_hashboards = 4
+        self.fan_count = 2
