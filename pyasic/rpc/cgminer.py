a0c76fe24f82cd3c66955cd75456df87e1a658d2
UpstreamData <brett@upstreamdata.ca>
2024-01-16 15:36:55 -0700

    refactor: remove unused imports.

--- pyasic/rpc/cgminer.py
+++ pyasic/rpc/cgminer.py
@@ -13,10 +13,8 @@
 #  See the License for the specific language governing permissions and         -
 #  limitations under the License.                                              -
 # ------------------------------------------------------------------------------
-import asyncio
-import logging
 
-from pyasic.rpc import APIError, BaseMinerRPCAPI
+from pyasic.rpc import BaseMinerRPCAPI
 
 
 class CGMinerRPCAPI(BaseMinerRPCAPI):
