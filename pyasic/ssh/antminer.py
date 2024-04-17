609f103992d7b485f2c67c4abc1725629ae34db6
1e9abhi1e10 <2311abhiptdr@gmail.com>
2024-04-17 17:01:38 +0530

    docs: Add documentation for `ssh`

--- pyasic/ssh/antminer.py
+++ pyasic/ssh/antminer.py
@@ -3,6 +3,12 @@ from pyasic.ssh.base import BaseSSH
 
 
 class AntminerModernSSH(BaseSSH):
+    """
+    Initialize an AntminerModernSSH instance.
+
+    Args:
+        ip (str): The IP address of the Antminer device.
+    """
     def __init__(self, ip: str):
         super().__init__(ip)
         self.pwd = settings.get("default_antminer_ssh_password", "root")
