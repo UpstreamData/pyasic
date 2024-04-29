46c56134f79b2ba7d3725d7d80916041fcb9aebb
Upstream Data <brett@upstreamdata.ca>
2024-04-29 09:14:08 -0600

    docs: update docs generation to fix marathon miners.

--- docs/generate_miners.py
+++ docs/generate_miners.py
@@ -47,8 +47,8 @@ def backend_str(backend: MinerTypes) -> str:
             return "Stock Firmware Goldshells"
         case MinerTypes.LUX_OS:
             return "LuxOS Firmware Miners"
-        case MinerTypes.EPIC:
-            return "ePIC Firmware Miners"
+        case MinerTypes.MARATHON:
+            return "Mara Firmware Miners"
 
 
 def create_url_str(mtype: str):
