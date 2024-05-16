afd8697f07b4e5124635e47153471a1175e5e239
John-Paul Compagnone <jpcompagnone@epicblockchain.io>
2024-05-15 21:03:12 -0400

    change from optimized to tuned, which is more generic for other FW

--- pyasic/config/mining.py
+++ pyasic/config/mining.py
@@ -252,7 +252,7 @@ class MiningModeHashrateTune(MinerConfigValue):
     hashrate: int = None
     throttle_limit: int = None
     throttle_step: int = None
-    optimized: bool = False
+    tuned: bool = False
     algo: TunerAlgo = field(default_factory=TunerAlgo.default)
 
     @classmethod
@@ -264,8 +264,8 @@ class MiningModeHashrateTune(MinerConfigValue):
             cls_conf["throttle_limit"] = dict_conf["throttle_limit"]
         if dict_conf.get("throttle_step"):
             cls_conf["throttle_step"] = dict_conf["throttle_step"]
-        if dict_conf.get("optimized"):
-            cls_conf["optimized"] = dict_conf["optimized"]
+        if dict_conf.get("tuned"):
+            cls_conf["tuned"] = dict_conf["tuned"]
         if dict_conf.get("algo"):
             cls_conf["algo"] = TunerAlgo.from_dict(dict_conf["algo"])
 
@@ -443,12 +443,13 @@ class MiningModeConfig(MinerConfigOption):
                         throttle_step=algo_info["VoltageOptimizer"].get(
                             "Throttle Step"
                         ),
-                        optimized=algo_info["VoltageOptimizer"].get("Optimized"),
+                        tuned=algo_info["VoltageOptimizer"].get("Optimized"),
                         algo=TunerAlgo.voltage_optimizer(),
                     )
                 else:
                     return cls.hashrate_tuning(
-                        hashrate=algo_info["ChipTune"]["Target"],
+                        hashrate=algo_info["ChipTune"].get("Target"),
+                        tuned=algo_info["ChipTune"].get("Optimized"),
                         algo=TunerAlgo.chip_tune(),
                     )
             else:
