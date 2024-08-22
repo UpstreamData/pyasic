1e5d1a252872f6cff46b5623aeefa0d988a3448d
James Hilliard <james.hilliard1@gmail.com>
2024-08-22 17:53:54 -0600

    Cleanup unused struct import

--- pyasic/settings/__init__.py
+++ pyasic/settings/__init__.py
@@ -13,7 +13,6 @@
 #  See the License for the specific language governing permissions and         -
 #  limitations under the License.                                              -
 # ------------------------------------------------------------------------------
-import struct
 from ssl import SSLContext
 from typing import Any, Union
 
