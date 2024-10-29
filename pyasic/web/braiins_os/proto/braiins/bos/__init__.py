d4665ed768caee305c4fa651826676ce1f0412eb
James Hilliard <james.hilliard1@gmail.com>
2024-10-29 12:14:47 -0600

    Update betterproto and regenerate protoc files
    Signed-off-by: James Hilliard <james.hilliard1@gmail.com>

--- pyasic/web/braiins_os/proto/braiins/bos/__init__.py
+++ pyasic/web/braiins_os/proto/braiins/bos/__init__.py
@@ -4,11 +4,16 @@
 # This file has been @generated
 
 from dataclasses import dataclass
-from typing import TYPE_CHECKING, Dict, Optional
+from typing import (
+    TYPE_CHECKING,
+    Dict,
+    Optional,
+)
 
 import betterproto
 import grpclib
 from betterproto.grpc.grpclib_server import ServiceBase
+
 
 if TYPE_CHECKING:
     import grpclib.server
