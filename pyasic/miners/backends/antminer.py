1508f7873a95adff1dd475d9506fcb37411e6eed
Upstream Data <brett@upstreamdata.ca>
2024-12-24 08:03:19 -0700

    bug: fix mining mde being set to an invalid `miner_mode` in antminer stock and hive

--- pyasic/miners/backends/antminer.py
+++ pyasic/miners/backends/antminer.py
@@ -184,13 +184,13 @@ class AntminerModern(BMMiner):
 
     async def stop_mining(self) -> bool:
         cfg = await self.get_config()
-        cfg.miner_mode = MiningModeConfig.sleep()
+        cfg.mining_mode = MiningModeConfig.sleep()
         await self.send_config(cfg)
         return True
 
     async def resume_mining(self) -> bool:
         cfg = await self.get_config()
-        cfg.miner_mode = MiningModeConfig.normal()
+        cfg.mining_mode = MiningModeConfig.normal()
         await self.send_config(cfg)
         return True
 
