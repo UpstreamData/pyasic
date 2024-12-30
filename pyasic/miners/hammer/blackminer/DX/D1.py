030a6653bc9aaffb73d6afab4c7e6b8ccf2082b5
John-Paul Compagnone <jpcompagnone@epicblockchain.io>
2024-12-30 16:04:08 -0500

    remove ref to D1

--- pyasic/miners/hammer/blackminer/DX/D1.py
+++
@@ -1,6 +0,0 @@
-from pyasic.miners.backends import BlackMiner
-from pyasic.miners.device.models import D1
-
-
-class HammerD1(BlackMiner, D1):
-    sticker_hashrate = 15150
--- pyasic/miners/hammer/blackminer/DX/__init__.py
+++ pyasic/miners/hammer/blackminer/DX/__init__.py
@@ -1,2 +1,1 @@
-from .D1 import HammerD1
 from .D10 import HammerD10
