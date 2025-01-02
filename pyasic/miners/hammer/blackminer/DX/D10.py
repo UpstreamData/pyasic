6dd6d72784b7bcb257f384b9b97000fc968c6794
John-Paul Compagnone <jpcompagnone@epicblockchain.io>
2025-01-02 00:36:15 -0500

    clean up hashrate for Hammer/Volcminer

--- pyasic/miners/hammer/blackminer/DX/D10.py
+++ pyasic/miners/hammer/blackminer/DX/D10.py
@@ -1,6 +1,8 @@
 from pyasic.miners.backends import BlackMiner
 from pyasic.miners.device.models import D10
+from pyasic.device.algorithm.scrypt import ScryptHashRate
+from pyasic.device.algorithm.hashrate.unit.scrypt import ScryptUnit
 
 
 class HammerD10(BlackMiner, D10):
-    sticker_hashrate = 5000000
+    sticker_hashrate = ScryptHashRate(rate=5.0, unit=ScryptUnit.GH)
--- pyasic/miners/volcminer/blackminer/DX/D1.py
+++ pyasic/miners/volcminer/blackminer/DX/D1.py
@@ -1,6 +1,8 @@
 from pyasic.miners.backends import BlackMiner
 from pyasic.miners.device.models import D1
+from pyasic.device.algorithm.scrypt import ScryptHashRate
+from pyasic.device.algorithm.hashrate.unit.scrypt import ScryptUnit
 
 
 class VolcMinerD1(BlackMiner, D1):
-    sticker_hashrate = 15150000
+    sticker_hashrate = ScryptHashRate(rate=15.15, unit=ScryptUnit.GH)
