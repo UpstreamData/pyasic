6566a078f471ce38f8154ec8c68712b5c7d4ca7f
timurey <taipov@gmail.com>
2025-04-02 10:42:39 +0500

    add expected_fans = 0 in asic description

--- pyasic/miners/device/models/antminer/X21/S21.py
+++ pyasic/miners/device/models/antminer/X21/S21.py
@@ -39,6 +39,7 @@ class S21PlusHydro(AntMinerMake):
     raw_model = MinerModel.ANTMINER.S21PlusHydro
 
     expected_chips = 95
+    expected_fans = 0
     expected_hashboards = 3
     algo = MinerAlgo.SHA256
 
