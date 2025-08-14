19dbb8624a92189d5cc2f8852bab865a6fde154c
Ryan Heideman <ryandh@outlook.com>
2025-08-13 18:45:42 -0700

    Fixed expected_hashrate

--- pyasic/miners/goldshell/bfgminer/MiniDoge/MiniDoge.py
+++ pyasic/miners/goldshell/bfgminer/MiniDoge/MiniDoge.py
@@ -13,13 +13,66 @@
 #  See the License for the specific language governing permissions and         -
 #  limitations under the License.                                              -
 # ------------------------------------------------------------------------------
+from typing import Optional
+
 from pyasic.config import MinerConfig
+from pyasic.device.algorithm import AlgoHashRate
 from pyasic.errors import APIError
 from pyasic.miners.backends import GoldshellMiner
+from pyasic.miners.data import (
+    DataFunction,
+    DataLocations,
+    DataOptions,
+    RPCAPICommand,
+    WebAPICommand,
+)
 from pyasic.miners.device.models import MiniDoge
 
 
+GOLDSHELL_MINI_DOGE_DATA_LOC = DataLocations(
+    **{
+        str(DataOptions.MAC): DataFunction(
+            "_get_mac",
+            [WebAPICommand("web_setting", "setting")],
+        ),
+        str(DataOptions.API_VERSION): DataFunction(
+            "_get_api_ver",
+            [RPCAPICommand("rpc_version", "version")],
+        ),
+        str(DataOptions.FW_VERSION): DataFunction(
+            "_get_fw_ver",
+            [WebAPICommand("web_status", "status")],
+        ),
+        str(DataOptions.HASHRATE): DataFunction(
+            "_get_hashrate",
+            [RPCAPICommand("rpc_summary", "summary")],
+        ),
+        str(DataOptions.EXPECTED_HASHRATE): DataFunction(
+            "_get_expected_hashrate",
+            [RPCAPICommand("rpc_devs", "devs")],
+        ),
+        str(DataOptions.HASHBOARDS): DataFunction(
+            "_get_hashboards",
+            [
+                RPCAPICommand("rpc_devs", "devs"),
+                RPCAPICommand("rpc_devdetails", "devdetails"),
+            ],
+        ),
+        str(DataOptions.FANS): DataFunction(
+            "_get_fans",
+            [RPCAPICommand("rpc_stats", "stats")],
+        ),
+        str(DataOptions.POOLS): DataFunction(
+            "_get_pools",
+            [RPCAPICommand("rpc_pools", "pools")],
+        ),
+    }
+)
+
+
 class GoldshellMiniDoge(GoldshellMiner, MiniDoge):
+
+    data_locations = GOLDSHELL_MINI_DOGE_DATA_LOC
 
     async def get_config(self) -> MinerConfig:
         try:
@@ -29,3 +82,23 @@ class GoldshellMiniDoge(GoldshellMiner, MiniDoge):
 
         self.config = MinerConfig.from_goldshell_list(pools)
         return self.config
+
+    async def _get_expected_hashrate(
+        self, rpc_devs: dict = None
+    ) -> Optional[AlgoHashRate]:
+        if rpc_devs is None:
+            try:
+                rpc_devs = await self.rpc.devs()
+            except APIError:
+                pass
+
+        if rpc_devs is not None:  
+            try:
+                hash_rate = rpc_devs["DEVS"][0]["estimate_hash_rate"]
+                return self.algo.hashrate(
+                    rate=float(hash_rate), unit=self.algo.unit.H
+                ).into(self.algo.unit.default)
+            except KeyError:
+                pass
+
+        return None
