7263a2190ca86b2c6185eaa385c75609af063cb4
Ryan Heideman <ryandh@outlook.com>
2025-08-26 20:02:37 -0700

    File renames

--- pyasic/miners/device/models/goldshell/Byte/__init__.py
+++ pyasic/miners/device/models/goldshell/Byte/__init__.py
@@ -13,4 +13,4 @@
 #  See the License for the specific language governing permissions and         -
 #  limitations under the License.                                              -
 # ------------------------------------------------------------------------------
-from .Byte import Byte
+from .byte import Byte
--- pyasic/miners/device/models/goldshell/__init__.py
+++ pyasic/miners/device/models/goldshell/__init__.py
@@ -13,8 +13,8 @@
 #  See the License for the specific language governing permissions and         -
 #  limitations under the License.                                              -
 # ------------------------------------------------------------------------------
-from .Byte import *
-from .MiniDoge import *
+from .byte import *
+from .mini_doge import *
 from .X5 import *
 from .XBox import *
 from .XMax import *
